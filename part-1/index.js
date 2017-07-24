b2ee6318854e35b5d2f60fd1af0627eeacf1b303
Ryan Kent <imsolost@gmail.com>
2017-07-24 11:31:57 -0700

    completed part 1

---
+++ part-1/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded({ extended: true }))
+
+app.get('/api/supported-operations', (req, res) => {
+  res.send({"/": "division",
+           "+": "addition",
+           "-": "subtration",
+           "*": "multiplication"})
+})
+
+app.get('/api/square', (req, res) => {
+  const num = req.query.number
+  res.send({"result": num * num})
+})
+
+app.post('/api/compute', (req, res) => {
+  const operator = req.body.operator
+  const operands = (req.body.operands).replace(/[\[\]']+/g,'').split(',')
+
+  if (operator === '-') {
+    res.send({"result": operands[0] - operands[1]})
+  }
+  if (operator === '*') {
+    res.send({"result": operands[0] * operands[1]})
+  }
+  if (operator === '/') {
+    res.send({"result": operands[0] / operands[1]})
+  }
+  if (operator === '+') {
+    res.send({"result": parseInt(operands[0]) + parseInt(operands[1]) })
+  }
+  else {
+    res.status(404).send({"error": "invalid operator ?. Valid operators are /, +, -, *"})
+  }
+
+})
+
+
+app.listen( 3000, () => {
+  console.log('API is listening on port 3000!')
+})
