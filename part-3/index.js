8b4d54a771d25d852a369893866523d29a67f643
Ryan Kent <imsolost@gmail.com>
2017-07-24 17:41:39 -0700

    building out layout

---
+++ part-3/index.js
@@ -0,0 +1,41 @@
+const cart = []
+const modal = document.querySelector('.modal')
+const itemslist = document.querySelector('.items-list')
+
+addToCart = (event) => {
+  const item = event.target.parentNode.innerText.split('\n')
+  cart.push({ item: item[0], price: Number( item[1].slice(1) ) })
+
+  itemslist.appendChild( document.createElement('div') ).className = "cart-row row"
+  const cartrows = document.querySelectorAll('.cart-row')
+  const lastrow = cartrows[cartrows.length -1]
+
+  lastrow.appendChild( document.createElement('p') ).innerText = item[0]
+  lastrow.appendChild( document.createElement('p') ).innerText = item[1]
+
+  cartCount()
+}
+
+sumPrice = (array) => {
+  let total = 0
+  for (let i = 0; i < array.length; i++) {
+    total = total + array[i].price
+  }
+  return total.toFixed(2)
+}
+
+cartCount = () => {
+    document.querySelector('.cart-item-count').innerText = `(${cart.length})`
+
+    // document.querySelector('.cart-total').innerText = 'Total: $' + sumPrice(cart)
+}
+
+toggleCart = () => {
+  if (modal.style.display === 'block') {
+    modal.style.display = 'none'
+  }
+  else modal.style.display = 'block'
+}
+
+document.querySelectorAll('.add-to-cart').forEach( element => element.addEventListener('click', addToCart) )
+document.querySelectorAll('.cart-button').forEach( element => element.addEventListener('click', toggleCart) )
